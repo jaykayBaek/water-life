c940e362ea55e5493233fa5841d8eb070356c1cf
jaykayBaek <jaykaybaek@gmail.com>
2023-04-17 14:49:50 +0900

    feat: separate login api from Member Controller
    로그인 api용 컨트롤러 생성, MemberController에서 분리

--- src/main/java/com/waterlife/controller/login/LoginController.java
+++ src/main/java/com/waterlife/controller/login/LoginController.java
@@ -1,39 +1,37 @@
-package com.waterlife.controller.member;
+package com.waterlife.controller.login;
 
 import com.waterlife.controller.ResultResponse;
+import com.waterlife.controller.member.MemberRequestResult;
 import com.waterlife.service.LoginForm;
-import com.waterlife.service.MemberRegisterForm;
 import com.waterlife.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/members")
 @RequiredArgsConstructor
-public class MemberRestController {
+public class LoginController {
 
     private final MemberService memberService;
 
-    @PostMapping("/register")
-    public ResponseEntity<ResultResponse> register(MemberRegisterForm form){
+    @PostMapping("/login")
+    public ResponseEntity<ResultResponse> login(LoginForm form){
 
-        memberService.register(form);
+        memberService.login(form);
 
         ResultResponse resultResponse = ResultResponse.builder()
-                .CODE(HttpStatus.CREATED.toString())
+                .CODE(HttpStatus.OK.toString())
                 .MESSAGE(MemberRequestResult.REGISTER_SUCCESS.getMessage())
                 .SUCCESS(true)
                 .build();
 
         return ResponseEntity
-                .status(HttpStatus.CREATED)
+                .status(HttpStatus.OK)
                 .body(resultResponse);
     }
-
 }
