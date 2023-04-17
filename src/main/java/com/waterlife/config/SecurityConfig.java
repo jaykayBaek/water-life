63bb2fe43027ed4583242513c6ac540f47cc9d72
jaykayBaek <jaykaybaek@gmail.com>
2023-04-17 18:03:45 +0900

    refactor: change security config setting

--- src/main/java/com/waterlife/config/SecurityConfig.java
+++ src/main/java/com/waterlife/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.waterlife.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,5 +11,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .formLogin().disable()
+                .headers().frameOptions().disable();
     }
 }
