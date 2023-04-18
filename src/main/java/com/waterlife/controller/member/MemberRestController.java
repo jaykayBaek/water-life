6be7b436c3504c5c0b8e2ced86788f57c86b8aa8
jaykayBaek <jaykaybaek@gmail.com>
2023-04-18 17:30:05 +0900

    feat: add loginId duplicate validation api
    프론트엔드에서 중복된 아이디인지 검증할 수 있는 api 추가

--- src/main/java/com/waterlife/controller/member/MemberRestController.java
+++ src/main/java/com/waterlife/controller/member/MemberRestController.java
@@ -1,16 +1,15 @@
 package com.waterlife.controller.member;
 
 import com.waterlife.controller.ResultResponse;
-import com.waterlife.service.LoginForm;
 import com.waterlife.service.MemberRegisterForm;
 import com.waterlife.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
@@ -21,13 +20,31 @@ public class MemberRestController {
     private final MemberService memberService;
 
     @PostMapping("/register")
-    public ResponseEntity<ResultResponse> register(MemberRegisterForm form){
+    public ResponseEntity<ResultResponse> register(
+            @ModelAttribute MemberRegisterForm form, HttpServletRequest request){
+        String lastVisitedIp = request.getRemoteAddr();
+        form.updateLastVisitedIp(lastVisitedIp);
 
         memberService.register(form);
 
         ResultResponse resultResponse = ResultResponse.builder()
                 .CODE(HttpStatus.CREATED.toString())
                 .MESSAGE(MemberRequestResult.REGISTER_SUCCESS.getMessage())
+                .SUCCESS(true)
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(resultResponse);
+    }
+
+    @PostMapping("/register/check/loginId")
+    public ResponseEntity<ResultResponse> checkLoginId(@RequestParam String loginId){
+        memberService.validateLoginId(loginId);
+
+        ResultResponse resultResponse = ResultResponse.builder()
+                .CODE(HttpStatus.OK.toString())
+                .MESSAGE(MemberRequestResult.NOT_DUPLICATED_LOGIN_ID.getMessage())
                 .SUCCESS(true)
                 .build();
 
