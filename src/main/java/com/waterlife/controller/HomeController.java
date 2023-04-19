f493bb6933adddf4021dbc72a19486ee36af6af5
jaykayBaek <jaykaybaek@gmail.com>
2023-04-19 15:54:31 +0900

    feat(front-end): add home login parts
    홈의 로그인 파트에서 세션 처리

--- src/main/java/com/waterlife/controller/HomeController.java
+++ src/main/java/com/waterlife/controller/HomeController.java
@@ -1,15 +1,30 @@
 package com.waterlife.controller;
 
+import com.waterlife.consts.SessionConst;
+import com.waterlife.entity.Member;
+import com.waterlife.repository.MemberRepository;
+import com.waterlife.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.HashMap;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
+@Slf4j
+@RequiredArgsConstructor
 public class HomeController {
+    private final MemberService memberService;
 
     @RequestMapping
-    public String home(){
+    public String home(@SessionAttribute(name = SessionConst.MEMBER_ID, required = false) Long memberId, Model model){
+
+        if(memberId != null){
+            MemberInformationResponse response = memberService.findMemberInformation(memberId);
+            model.addAttribute("member", response);
+        }
+
         return "home/home";
     }
 }
